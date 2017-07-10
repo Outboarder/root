9aa13404b65f497d01f598eec2e22dda405246d5
Outboarder <pooyapayvar@gmail.com>
2017-07-11 02:10:10 +0430

    oo

---
+++ YAYA/EXT - Set Input Channel (Mono)(3).lua
@@ -0,0 +1,17 @@
+local filename = ({reaper.get_action_context()})[2]
+Input_Mono = tonumber(filename:match("%((%d+)%).lua$"))
+
+
+Count_Tracks = reaper.CountSelectedTracks(0)
+for i = 0 ,Count_Tracks-1 do
+  
+    Media_Track = reaper.GetSelectedTrack(0,i) 
+    if Media_Track ~= nil then
+    
+    reaper.SetMediaTrackInfo_Value( Media_Track, 'I_RECINPUT' , Input_Mono-1 )    
+    --reaper.SetMediaTrackInfo_Value( Media_Track, 'I_RECINPUT' , Input_Stereo+1023 )  
+  end
+end
+                                 
+                 
+
---
+++ YAYA/EXT - Set Input Channel.lua
@@ -0,0 +1,30 @@
+--[[
+@description Set Input Channel (Mono)
+@version 1.0
+@author Outboarder
+@metapackage
+@provides
+
+[main] . > EXT - Set Input Channel (Mono)(1).lua
+[main] . > EXT - Set Input Channel (Mono)(2).lua
+[main] . > EXT - Set Input Channel (Mono)(3).lua
+[main] . > EXT - Set Input Channel (Mono)(4).lua
+[main] . > EXT - Set Input Channel (Mono)(5).lua
+
+--]]
+
+local filename = ({reaper.get_action_context()})[2]
+Input_Mono = tonumber(filename:match("%((%d+)%).lua$"))
+
+-- script code
+Count_Tracks = reaper.CountSelectedTracks(0)
+for i = 0 ,Count_Tracks-1 do
+  
+    Media_Track = reaper.GetSelectedTrack(0,i) 
+    if Media_Track ~= nil then
+    
+    reaper.SetMediaTrackInfo_Value( Media_Track, 'I_RECINPUT' , Input_Mono-1 )    
+  end
+end
+
+
